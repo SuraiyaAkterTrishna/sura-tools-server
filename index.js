60a0f9d8ee742afd6925d04e27b3325d03474e1e
Suraiya  Akter Trishna <suraiyaaktertrishna@gmail.com>
2022-09-08 09:39:09 +0600

    test 1

--- index.js
+++ index.js
@@ -8,12 +8,15 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const { header } = require("express/lib/request");
 const port = process.env.PORT || 5000;
 
+
 // middleware
 app.use(cors());
 app.use(express.json());
 
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7wspt6d.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
@@ -29,6 +32,7 @@ function verifyJWT(req, res, next) {
       next();
     });
   }
+
 
 async function run() {
   try {
